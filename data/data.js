24fc2d5aa56f1a1bb1263ae262d4932d77b3aa6f
Daniel Lazarotto <40578508+daniellazarotto@users.noreply.github.com>
2020-12-16 16:28:28 -0300

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,77 @@
+/*!
+ * Guim González - Portfolio v1.0.0 (https://guimgonzalez.me)
+ * Copyright 2019 Guim González
+ * Licensed under MIT (https://github.com/GuimG/dev-portfolio/master/LICENSE)
+ */
+
+// Change your profile data here
+let profile = {
+  picture: "",
+  name: "Daniel Lazarotto",
+  description: "Desenvolvedor Front End",
+  bio:
+    " Ainda vai ser feito confia",
+  email: "daniel_lazar_otto@hotmail.com",
+  location: "Erechim, Brasil",
+  birth: "1997-12-4", // YYYY-MM-DD format
+
+
+  skills: [
+    "HTML5",
+    "Javascript",
+    "LESS/SASS",
+    "C/C++",
+    "GIT",
+    "VSCODE"
+  ],
+  social: {
+    linkedin: "https://www.linkedin.com/in/daniel-lazarotto-8b7a08ab/",
+    instagram: "http://instagram.com/daniellazarotto",
+    github: "https://github.com/daniellazarotto",
+  },
+  experience: [
+    {
+      title: "Jovem Aprendiz",
+      company: "Sicredi (Sistema de Crédito Cooperativo)",
+      url: "https://www.sicredi.com.br/site/home",
+      location: "Barra do Rio Azul, Brasil",
+      description:
+        "",
+      from: "Fev. 2013",
+      to: "Jan. 2014"
+    },
+    {
+      title: "Desenvolvedor",
+      company: "Compasso Tecnologia LTDA",
+      url: "https://compassouol.com/",
+      location: "Erechim, Brasil",
+      description:
+        "",
+      from: "Jul. 2017",
+      to: "Até o momento"
+    }
+  ],
+  education: [
+    {
+      school: "Universidade Regional Integrada do Alto Uruguai e das Missões (URI Erechim)",
+      degree: "Bacharel em Ciência da Computação",
+      fieldOfStudy: "Ciência da Computação",
+      location: "Erechim, Brasil",
+      from: "Jan. 2015",
+      to: "Dez. 2019"
+    }
+  ],
+  quote: "It's just a code there"
+};
+
+let page = {
+  language: "english",
+  // ^ languages suported:
+  //  - english (default)
+  theme: "nightsky"
+  // ^ themes suported:
+  //  - nightsky (default)
+  //  - sunset
+  //  - purpledream
+  //  - cutekitty
+};
